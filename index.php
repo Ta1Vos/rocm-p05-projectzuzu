806b4d3dff2bcdb03aee786e5c471292be86e31c
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-09-07 09:31:54 +0200

    improved navbar layout

--- index.php
+++ index.php
@@ -6,30 +6,36 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>ZuZu | Home</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">ZuZu</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="sushi_orders.php">Sushi</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="customer_info.php">Klantgegevens</a>
-                </li>
-            </ul>
-        </div>
+<header>
+    <div class="container-fluid ps-0">
+        <img class="vw-100 vh-5" src="img/sushi_header.png" alt="Afbeelding van sushi">
     </div>
-</nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="row container-fluid ps-0 ps-3">
+            <div class="col-4"></div>
+            <a class="col-1 navbar-brand ps-3" href="#">ZuZu</a>
+            <div class="col-1 collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sushi_orders.php">Sushi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="customer_info.php">Klantgegevens</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-5"></div>
+        </div>
+    </nav>
+</header>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
