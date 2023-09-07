fb181ecf52aa1fa23639fe07c295277755a49859
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-09-07 09:59:16 +0200

    Added home text

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>ZuZu | Home</title>
 </head>
@@ -39,13 +40,35 @@
     </nav>
 </header>
 <main>
+    <div class="row text-center">
+        <div class="col-4"></div>
+        <div class="col-4">
+            <h2>
+                Goedemiddag, welkom bij ZuZu
+            </h2>
+            <small>
+                Wij zijn gespecialiseerd in de Japanse keuken.<br>
+                Het woord "sushi" is afkomstig van "su", wat azijn betekent en "shi" rijst.
+            </small>
+            <p class="fs-5 fw-bold">
+                Vandaag dinsdag 7 september 2023
+            </p>
+            <p class="fs-6 fw-bold">
+                Bezorgtijd: 17.00-21.00<br>
+                Openingstijd: 12.00-21.00
+            </p>
+        </div>
+        <div class="col-4"></div>
 
+    </div>
 </main>
 <footer>
 
 </footer>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
 </body>
 </html>
