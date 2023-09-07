e296b4ef36db5f37915a5da45fdc225bdadb6606
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-09-07 10:23:26 +0200

    Added footer content

--- index.php
+++ index.php
@@ -82,7 +82,33 @@
     </div>
 </main>
 <footer>
-
+    <div class="row vh-20 bg-dark text-white text-center">
+        <div class="col-2"></div>
+        <div class="col-2">
+            <h6>Contactgegevens</h6>
+            <small>
+                Restaurant ZuZu<br>
+                Appelstraat 1<br>
+                1111AA Fruit<br>
+                zuzu@gmail.com<br>
+                +06-12345678
+            </small>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-2">
+            <h6>Openingstijden</h6>
+            <small>
+                Maandag: Gesloten<br>
+                Dinsdag: Gesloten<br>
+                Woensdag: 16.00-20.00<br>
+                Donderdag: 16.00-20.00<br>
+                Vrijdag: 15.00-21.00<br>
+                Zaterdag: 12.00-21.00<br>
+                Zondag: 12.00-20.00<br>
+            </small>
+        </div>
+        <div class="col-2"></div>
+    </div>
 </footer>
 
 
