07d27cf9532dad1b2fdba060afb7e8eafbb27167
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-10-24 21:53:22 +0200

    Added amount update, added order confirm

--- order_overview.php
+++ order_overview.php
@@ -4,44 +4,77 @@
     $receipt = null;
     $receiptTotal = null;
     $customerInfoDiv = null;
+    $confirmButton = null;
 
-    if ($_SESSION['customer-info']) {
-        $customerArray = unserialize($_SESSION['customer-info']);
+    if (!isset($_POST['confirm-order'])) {
+        //If there hasn't been an order confirmation yet
+        if (isset($_SESSION['customer-info'])) {
+            $customerArray = unserialize($_SESSION['customer-info']);
 
-        $customerInfoDiv = "{$customerArray[0]} {$customerArray[1]}<br> {$customerArray[3]}<br> {$customerArray[4]} {$customerArray[5]}<br> {$customerArray[2]}"; //Loads customer information
+            $customerInfoDiv = "{$customerArray[0]} {$customerArray[1]}<br> {$customerArray[3]}<br> {$customerArray[4]} {$customerArray[5]}<br> {$customerArray[2]}"; //Loads customer information
 
-        //Checks if any sushi has already been selected on the sushi page
-        if (isset($_SESSION['receipt'])) {
-            $totalPrice = 0;
-            $row = 0;
+            //Checks if any sushi has already been selected on the sushi page
+            if (isset($_SESSION['receipt'])) {
+                $totalPrice = 0;
+                $row = 0;
 
-            //Loops through the session and echoes the sushi, prices and amounts
-            foreach ($_SESSION['receipt'] as $receiptLine) {
-                //Checks if a row has been selected to be deleted
-                if (isset($_POST['remove-row-' . $row])) {
-                    unset($_SESSION['receipt'][$row]); //Removes a row
-                    $_SESSION['receipt'] = array_values($_SESSION['receipt']); //Restores array indexes
-                    header("Refresh:0"); //Refreshes page so the receipt does not break
+                //Loops through the session and echoes the sushi, prices and amounts
+                foreach ($_SESSION['receipt'] as $receiptLine) {
+                    //Checks if a row has been selected to be deleted
+                    if (isset($_POST['remove-row-' . $row])) {
+                        unset($_SESSION['receipt'][$row]); //Removes a row
+                        $_SESSION['receipt'] = array_values($_SESSION['receipt']); //Restores array indexes
+                        header("Refresh:0"); //Refreshes page so the receipt does not break
+                    }
+                    //Shortcuts to array items
+                    $product = $receiptLine[0];
+                    $amount = $receiptLine[1];
+                    $price = $product["price"];
+                    //Adds a row to the receipt
+                    $receipt .= "&nbsp; <form method='post' class='row'><div class='col-7'><input type='submit' name='remove-row-{$row}' class='btn btn-danger' value='X'>&nbsp;&nbsp;";
+
+                    $price = $amount * $price; //Calculates the total price
+
+                    $receipt .= "{$product["name"]}</div><div class='col-3'></div><div class='col-2 text-end'>{$amount}x | &euro;" . number_format($price, 2, ",", ".") . "</div></form> <br>";
+                    $totalPrice += $amount * $product["price"];
+                    $row++; //Adds up 1 row as a new one will be created
                 }
-                //Shortcuts to array items
-                $product = $receiptLine[0];
-                $amount = $receiptLine[1];
-                $price = $product["price"];
-                //Adds a row to the receipt
-                $receipt .= "&nbsp; <form method='post' class='row'><div class='col-7'><input type='submit' name='remove-row-{$row}' class='btn btn-danger' value='X'>&nbsp;&nbsp;";
 
-                $price = $amount * $price; //Calculates the total price
+                //Shows order button after there is at least something to be purchased
+                if ($totalPrice > 0) {
+                    $confirmButton .= "<input type='submit' name='confirm-order' value='Bestellen' class='btn btn-dark'>";
+                }
 
-                $receipt .= "{$product["name"]}</div><div class='col-3'></div><div class='col-2 text-end'>{$amount}x | &euro;" . number_format($price, 2, ",", ".") . "</div></form> <br>";
-                $totalPrice += $amount * $product["price"];
-                $row++; //Adds up 1 row as a new one will be created
+                //Shows the total price of the receipt
+                $receiptTotal = "&euro;" . number_format($totalPrice, 2, ",", ".");
             }
-
-            //Shows the total price of the receipt
-            $receiptTotal = "&euro;" . number_format($totalPrice, 2, ",", ".");
+        } else {
+            $customerInfoDiv = "U bent helaas niet ingelogd of u heeft nog geen gegevens doorgegeven. Hierdoor kunt u nog niets bestellen.";
         }
     } else {
-        $customerInfoDiv = "U bent helaas niet ingelogd of u heeft nog geen gegevens doorgegeven. Hierdoor kunt u nog niets bestellen.";
+        //If the order has been confirmed
+        if (!isset($_SESSION['receipt'])) {// Small failsafe in case the session doesn't exist for some reason
+            $_SESSION['receipt'] = array();
+            header("Location: http://localhost/sd22-p5-projectzuzu-Ta1Vos/index.php");
+        } else {
+            include("db_connection.php");
+            global $db;
+
+            foreach ($_SESSION['receipt'] as $productLine) {
+                try {
+                    $product = $productLine[0];
+                    $query = $db->prepare("UPDATE sushi SET available_amount = " . $product["available_amount"] - $productLine[1] . " WHERE id = " . $product["id"]);
+                    $query->execute();
+                } catch (PDOException $error) {
+                    die("Oh oh! Er is iets fout gegaan! Error code: " . $error->getMessage());
+                }
+            }
+
+            unset($_SESSION['receipt']);
+            unset($_POST['confirm-order']);
+            $receipt = "<h2 class='text-center'>Uw bestelling is doorgegeven!</h2>";
+            $confirmButton = "<small>(Dit is geen fictieve bestelwebsite, er wordt helaas NIETS bezorgd)</small>";
+        }
     }
 ?>
 
@@ -97,9 +130,11 @@
                     <p class="order-overview-result">
                         <?= $receipt; ?>
                     </p>
-                    <p>
-                        <h5 class="text-end">Totaal: <?= $receiptTotal; ?></h5>
-                    </p>
+                    <form method="post" class="row">
+                        <span class="col-4"></span>
+                        <span class="col-4 text-center"><?= $confirmButton; ?></span>
+                        <h5 class="col-4 text-end">Totaal: <?= $receiptTotal; ?></h5>
+                    </form>
                 </div>
             </div>
             <div class="card p-2 ps-3">
