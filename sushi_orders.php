87666b401664424493cb83eb8b173094e57fd699
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-10-26 09:07:30 +0200

    Added include for footer

--- sushi_orders.php
+++ sushi_orders.php
@@ -134,33 +134,9 @@ $divContent .= "</div>";
     </div>
 </main>
 <footer>
-    <div class="row vh-20 bg-dark text-white text-center p-3 mt-5">
-        <div class="col-2"></div>
-        <div class="col-2">
-            <h6>Contactgegevens</h6>
-            <small>
-                Restaurant ZuZu<br>
-                Appelstraat 1<br>
-                1111AA Fruit<br>
-                zuzu@gmail.com<br>
-                +06-12345678
-            </small>
-        </div>
-        <div class="col-4"></div>
-        <div class="col-2">
-            <h6>Openingstijden</h6>
-            <small>
-                Maandag: Gesloten<br>
-                Dinsdag: Gesloten<br>
-                Woensdag: 16.00-20.00<br>
-                Donderdag: 16.00-20.00<br>
-                Vrijdag: 15.00-21.00<br>
-                Zaterdag: 12.00-21.00<br>
-                Zondag: 12.00-20.00<br>
-            </small>
-        </div>
-        <div class="col-2"></div>
-    </div>
+    <footer>
+        <?php include("footer.php"); ?>
+    </footer>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
